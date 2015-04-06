49dc8e423f973c6f9dc96cf4eb7d3dd228861582
Paul Day <pday02@mail.bkk.ac.uk>
2015-04-06 18:40:54 +0100

    First commit.

---
+++ TestWebCrawler.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestWebCrawler {
+
+	/*
+	 * Things to test:
+	 * 	-URL isnt valid
+	 * 	-File already exists
+	 * 	-if neither defined then don't run
+	 * 
+	 * 
+	 */
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
