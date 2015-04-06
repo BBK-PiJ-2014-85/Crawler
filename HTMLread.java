703c7239dbf021b7c90e9a0cb351d445e4791734
Paul Day <pday02@mail.bkk.ac.uk>
2015-04-06 14:42:33 +0100

    Added method and javadoc for skipSpace()

--- HTMLread.java
+++ HTMLread.java
@@ -18,6 +18,7 @@ public class HTMLread {
 	 * 
 	 * TODO: Determine if the character itself is consumed as well (should become clear later). Assumed yes for now.
 	 * TODO: What to return if an end file is found at the end? Currently return false when find -1 (eof)
+	 * TODO: What id paramters the same? Currently return ch1 
 	 * 
 	 * @param stream the input stream
 	 * @param ch1 the first character to match
@@ -41,8 +42,32 @@ public class HTMLread {
 		return false;
 	}
 	
+	/**
+	 * Consumes an input stream until a non whitespace character is consumed. If this character is not the same as the input character,
+	 * the character is returned, else the lowest character value is provided.
+	 * 
+	 * TODO: What to do if the end of file is found? Currently return lowest as well. May not be most practical.
+	 * TODO: What if the char is actually whitespace? Currently it still returns lowest.
+	 * TODO: Case sensitive? Assumed that it is. 
+	 * 
+	 * @param stream the input stream
+	 * @param ch the character which returns will lowest character value
+	 * @return the non whitespace character encountered or the lowest character value should it match the input parameter
+	 */
 	public static char skipSpace(InputStream stream, char ch)
 	{
+		int n;
+		
+		try {
+			while ((n = stream.read()) != -1)
+			{
+				if ((char) n == ch) return Character.MIN_VALUE;
+				else if (!Character.isWhitespace(n)) return (char) n;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		return Character.MIN_VALUE;
 	}
 	
@@ -60,7 +85,14 @@ public class HTMLread {
 		System.out.println("Should be true: " + HTMLread.readUntil(in,'H','i'));
 		System.out.println("Should be false: " + HTMLread.readUntil(in,'H','i'));
 		System.out.println("Should be s: " + (char)in.read());
-
+		System.out.println("Should be false: " + HTMLread.readUntil(in,'z','p'));
+		
+		String test2 = "  A        b             c";
+		InputStream in2 = new ByteArrayInputStream(test2.getBytes());
+		System.out.println("Should be null: " + HTMLread.skipSpace(in2, 'A'));
+		System.out.println("Should be b: " + HTMLread.skipSpace(in2, 'A'));
+		System.out.println("Should be c: " + HTMLread.skipSpace(in2, 'A'));
+		System.out.println("Should be null: " + HTMLread.skipSpace(in2, 'A'));
 		
 	}
 	
