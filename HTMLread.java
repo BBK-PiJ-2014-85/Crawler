7a8c40538256979f99d22bf21ed14088bcdb59a9
Paul Day <pday02@mail.bkk.ac.uk>
2015-04-06 12:12:49 +0100

    Added base file with empty shell methods

---
+++ HTMLread.java
@@ -0,0 +1,21 @@
+import java.io.InputStream;
+
+
+public class HTMLread {
+
+	public static boolean readUntil(InputStream stream, char ch1, char c2)
+	{
+		return true;
+	}
+	
+	public static char skipSpace(InputStream stream, char ch)
+	{
+		return Character.MIN_VALUE;
+	}
+	
+	public static String readString(InputStream stream, char c1, char c2)
+	{
+		return null;
+	}
+	
+}
