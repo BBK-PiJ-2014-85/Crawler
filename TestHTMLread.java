84ac6c437f7f1e58edd81a10bdf34d04526ac77e
Paul Day <pday02@mail.bkk.ac.uk>
2015-04-06 14:20:43 +0100

    Added beginnings of roughtests to produce

---
+++ TestHTMLread.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestHTMLread {
+
+	/*
+	 * Things which should be tested
+	 * 
+	 * readUntil:
+	 * 	- not case sensitive
+	 * 	- work on empty input
+	 *  - returns false if eof
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
