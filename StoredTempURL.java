d77322d45edc037b35375c2f3c752c906e86e17d
Paul Day <pday02@mail.bkk.ac.uk>
2015-04-12 23:03:46 +0100

    First commit.

---
+++ StoredTempURL.java
@@ -0,0 +1,15 @@
+import java.net.URL;
+
+
+public class StoredTempURL {
+
+	public int priority;
+	public URL url;
+	
+	public StoredTempURL(int priority, URL url)
+	{
+		this.priority = priority;
+		this.url = url;
+	}
+	
+}
