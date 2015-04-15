a38e19c0daef62730bdaee38d014d5ca2affcbfa
Paul Day <pday02@mail.bkk.ac.uk>
2015-04-15 23:03:10 +0100

    Added functionality that it records searches made when test database has been added

--- HTMLStream.java
+++ HTMLStream.java
@@ -2,12 +2,14 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
 /**
- * This class returns the stream of a URL. It can also have a url database fed into it which it will refer to instead, rather than connecting to http://. This
- * is useful for things such as testing;
+ * This class returns the stream of a URL. It can also have a url database fed into it which it will refer to instead, rather than connecting to http://. Further, should
+ * a test database be set, it will also store the list of URLs which have been searched. This functionality makes testing the crawler easier.
  * 
  * @author Paul Day
  */
@@ -15,6 +17,7 @@ import java.util.Map;
 public class HTMLStream {
 
 	private static Map<URL,String> pages = null;
+	private static List<URL> searchedURLs = new ArrayList<URL>();
 	
 /**
  * Sets the map of URL and corresponding return pages.
@@ -25,7 +28,8 @@ public class HTMLStream {
 	public static void addTestURLs(Map<URL,String> testPages) {pages = testPages;}
 
 	/**
-	 * Returns the stream from an input URL, either from an http: connection or of a set page should a testing urls have been defined
+	 * Returns the stream from an input URL, either from an http: connection or of a set page should a testing urls have been defined. Should a test database be defined,
+	 * the search will be recorded in the database.
 	 * 
 	 * @param url the url of which to open the stream of
 	 * @return the stream of the URL, and the stream of the string page should a testing map have been added
@@ -34,17 +38,30 @@ public class HTMLStream {
 	
 	public static InputStream getStream(URL url) throws IOException
 	{
-		return (pages == null ? url.openStream() : new ByteArrayInputStream(pages.get(url).getBytes()));
+		if (pages == null) return url.openStream();
+		else 
+		{
+			searchedURLs.add(url);
+			return new ByteArrayInputStream(pages.get(url).getBytes());
+		}
 	}
 	
 	/**
-	 * Resets the stream to no longer use a test database but rather return the stream from the http: connection.
+	 * Resets the stream to no longer use a test database but rather return the stream from the http: connection. It also resets the search history of the crawler.
 	 */
 	
 	public static void clearSetURLS() 
 	{
 		pages=null;
+		searchedURLs = new ArrayList<URL>();
 	}
 	
+	/**
+	 * Returns the list of recorded URLs that have been searched. Useful for testing functionality of the WebCrawler.
+	 * 
+	 * @return a list of URLs that were searched, in the order in which they were searched.
+	 */
+	
+	public static List<URL> getSearchedURLs() {return searchedURLs;}
 	
 }
