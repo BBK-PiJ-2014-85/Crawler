52a2621472a96a265d371177c4f8f8ae45483f5e
Paul Day <pday02@mail.bkk.ac.uk>
2015-04-25 20:58:25 +0100

    First commit.

---
+++ StreamHolder.java
@@ -0,0 +1,13 @@
+import java.io.InputStream;
+
+
+/**
+ * This class is just a wrapper holding the Stream (if connection accepted) and response code from the connection.
+ * 
+ * @author Paul Day
+ *
+ */
+public class StreamHolder {
+		int response;
+		InputStream stream;
+}
